b17c7fc21e521c9320c7e2bdbe11b8985d51785e
Yaroslava <yarakucherenko07@gmail.com>
2024-12-06 20:08:39 +0200

    fix-env-variables

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,8 @@
-// @ts-check
-const { defineConfig, devices } = require('@playwright/test');
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+const { defineConfig, devices } = require('@playwright/test');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -27,11 +24,8 @@ module.exports = defineConfig({
     baseURL: process.env.BASE_URL,
     httpCredentials: {
       username: process.env.HTTP_USERNAME,
-      password: process.env.HTTP_PASSWORD
-    }
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
+      password: process.env.HTTP_PASSWORD,
+    },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -42,37 +36,16 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-/*
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
     */
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
